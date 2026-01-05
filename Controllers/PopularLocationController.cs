842fa5999aa61a86dcef6fd30cea7dfca38f2f17
Ozkan Karatas <ozkan@kurumsal.io>
2026-01-06 00:40:32 +0300

    PopularLocations and Consume

---
+++ Controllers/PopularLocationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.PopularLocationRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PopularLocationController : ControllerBase
+    {
+        private readonly IPopularLocationRepository _popularLocationRepository;
+        public PopularLocationController(IPopularLocationRepository popularLocationRepository)
+        {
+            _popularLocationRepository = popularLocationRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PopularLocationList()
+        {
+            var value = await _popularLocationRepository.GetAllPopularLocationAsyn();
+            return Ok(value);
+        }
+    }
+}
---
+++ Repositories/PopularLocationRepository/PopularLocationRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_Api.Dtos.PopularLocationDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepository
+{
+    public class PopularLocationRepository : IPopularLocationRepository
+    {
+        private readonly Context _context;
+        public PopularLocationRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultPopularLocationDto>> GetAllPopularLocationAsyn()
+        {
+            string query = "SELECT * FROM PopularLocation";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultPopularLocationDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
