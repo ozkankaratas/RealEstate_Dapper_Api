ccb71a061785166c156dd5a003b2bc4113cf8d35
Ozkan Karatas <ozkanqaratas@gmail.com>
2026-01-18 16:12:00 +0300

    Contact(Messages) API and Consuming

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.ContactDtos;
+using RealEstate_Dapper_Api.Repositories.ContactRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactRepository _contactRepository;
+        public ContactsController(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllContactAsyn()
+        {
+            var values = await _contactRepository.GetAllContactAsyn();
+            return Ok(values);
+        }
+
+        [HttpGet("GetLastFourContact")]
+        public async Task<IActionResult> GetLastFourContact()
+        {
+            var values = await _contactRepository.GetLastFourContact();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContact(int id)
+        {
+            var values = await _contactRepository.GetContact(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+        {
+            _contactRepository.CreateContact(createContactDto);
+            return Ok("Başarıyla Oluşturuldu");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteContact(int id)
+        {
+            _contactRepository.DeleteContact(id);
+            return Ok("Başarıyla Silindi");
+        }
+    }
+}
---
+++ Repositories/ContactRepositories/ContactRepository.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ContactDtos;
+using RealEstate_Dapper_Api.Dtos.EmployeeDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ContactRepositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly Context _context;
+        public ContactRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async void CreateContact(CreateContactDto createContactDto)
+        {
+            string query = "INSERT INTO Contact (Name, Email, Subject, Message, SendDate) VALUES (@name, @email, @subject, @message, @sendDate)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@name", createContactDto.Name);
+            parameters.Add("@email", createContactDto.Email);
+            parameters.Add("@subject", createContactDto.Subject);
+            parameters.Add("@message", createContactDto.Message);
+            parameters.Add("@sendDate", createContactDto.SendDate);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeleteContact(int id)
+        {
+            string query = "DELETE FROM Contact WHERE ContactID = @contactID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@contactID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<List<ResultContactDto>> GetAllContactAsyn()
+        {
+            string query = "SELECT * From Contact";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultContactDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIDContactDto> GetContact(int id)
+        {
+            string query = "SELECT * FROM Contact WHERE ContactID = @contactID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@contactID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIDContactDto>(query, parameters);
+                return values;
+            }
+        }
+
+        public async Task<List<LastFourContactResultDto>> GetLastFourContact()
+        {
+            string query = "SELECT TOP 4 * From Contact ORDER BY SendDate DESC";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<LastFourContactResultDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
