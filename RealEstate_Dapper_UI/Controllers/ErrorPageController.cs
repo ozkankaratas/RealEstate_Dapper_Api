20287937f87439bb311aab32b7f69761c1168a04
Ozkan Karatas <ozkanqaratas@gmail.com>
2026-01-20 00:46:16 +0300

    404 Error Page Update

---
+++ RealEstate_Dapper_UI/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+            return View();
+        }
+    }
+}
