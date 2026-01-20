5e0ff41e2657f191968f098f9321138661b237c2
Ozkan Karatas <ozkanqaratas@gmail.com>
2026-01-20 17:10:49 +0300

    ErrorPages Updates

--- RealEstate_Dapper_UI/Controllers/ErrorPageController.cs
+++ RealEstate_Dapper_UI/Controllers/ErrorPageController.cs
@@ -4,9 +4,32 @@ namespace RealEstate_Dapper_UI.Controllers
 {
     public class ErrorPageController : Controller
     {
-        public IActionResult Error404(int code)
+        // 1. ÖZEL KAPI: Sadece 500 Hataları İçin
+        [Route("ErrorPage/ServerError")]
+        public IActionResult ServerError()
         {
-            return View();
+            ViewBag.Message = "Sunucuda işler karıştı, müdahale ediyoruz.";
+            return View("Error500");
+        }
+
+        // 2. GENEL KAPI: 404, 403 vb. için
+        [Route("ErrorPage/Index")]
+        public IActionResult Index([FromQuery] int code)
+        {
+            ViewBag.Code = code;
+
+            if (code == 404)
+            {
+                ViewBag.Message = "Aradığın sayfayı bulamadık ama bir oyun bulduk!";
+                return View("Error404");
+            }
+            else if (code == 401 || code == 403)
+            {
+                ViewBag.Message = "Buraya girmek için yetkiniz yok!";
+                return View("AccessDenied");
+            }
+
+            return View("DefaultError");
         }
     }
 }
--- RealEstate_Dapper_UI/Program.cs
+++ RealEstate_Dapper_UI/Program.cs
@@ -1,18 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // Test ortamında detaylı hata sayfası
+    app.UseDeveloperExceptionPage();
+
+    // Canlı ortamda özel hata sayfasına yönlendirme
+    //app.UseExceptionHandler("/ErrorPage/ServerError");
+}
+else
+{
+    app.UseExceptionHandler("/ErrorPage/ServerError");
     app.UseHsts();
 }
+
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Index", "?code={0}");
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -20,6 +28,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
