de86829b118175a66bf36d6d5029d849cf777846
Ozkan Karatas <ozkanqaratas@gmail.com>
2026-02-25 02:19:25 +0300

    Extensions for Program.cs

---
+++ Containers/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Repositories.AppUserRepositories;
+using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+using RealEstate_Dapper_Api.Repositories.ContactRepositories;
+using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
+using RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories;
+using RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticsRepositories;
+using RealEstate_Dapper_Api.Repositories.LocationRepositories;
+using RealEstate_Dapper_Api.Repositories.MessageRepositories;
+using RealEstate_Dapper_Api.Repositories.PopularLocationRepository;
+using RealEstate_Dapper_Api.Repositories.ProductImageRepositories;
+using RealEstate_Dapper_Api.Repositories.ProductRepository;
+using RealEstate_Dapper_Api.Repositories.PropertyAmenityRepositories;
+using RealEstate_Dapper_Api.Repositories.ServiceRepository;
+using RealEstate_Dapper_Api.Repositories.StatisticsRepositories;
+using RealEstate_Dapper_Api.Repositories.SubFeatureRepositories;
+using RealEstate_Dapper_Api.Repositories.TestimonialRepositories;
+using RealEstate_Dapper_Api.Repositories.ToDoListRepositories;
+using RealEstate_Dapper_Api.Repositories.WhoWeAreDetailRepository;
+
+namespace RealEstate_Dapper_Api.Containers
+{
+    public static class Extensions
+    {
+        public static void ContainerDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<Context>();
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+            services.AddTransient<IServiceRepository, ServiceRepository>();
+            services.AddTransient<IBottomGridRepository, BottomGridRepository>();
+            services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+            services.AddTransient<ITestimonialRepository, TestimonialRepository>();
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            services.AddTransient<IStatisticsRepository, StatisticsRepository>();
+            services.AddTransient<IContactRepository, ContactRepository>();
+            services.AddTransient<IToDoListRepository, ToDoListRepository>();
+            services.AddTransient<IStatisticRepository, StatisticRepository>();
+            services.AddTransient<IChartRepository, ChartRepository>();
+            services.AddTransient<IMessageRepository, MessageRepository>();
+            services.AddTransient<IProductImageRepository, ProductImageRepository>();
+            services.AddTransient<IAppUserRepository, AppUserRepository>();
+            services.AddTransient<IPropertyAmenityRepository, PropertyAmenityRepository>();
+            services.AddTransient<ILocationRepository, LocationRepository>();
+            services.AddTransient<ISubFeatureRepository, SubFeatureRepository>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,5 @@
+using RealEstate_Dapper_Api.Containers;
 using RealEstate_Dapper_Api.Hubs;
-using RealEstate_Dapper_Api.Models.DapperContext;
-using RealEstate_Dapper_Api.Repositories.AppUserRepositories;
-using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
-using RealEstate_Dapper_Api.Repositories.CategoryRepository;
-using RealEstate_Dapper_Api.Repositories.ContactRepositories;
-using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
-using RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories;
-using RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticsRepositories;
-using RealEstate_Dapper_Api.Repositories.LocationRepositories;
-using RealEstate_Dapper_Api.Repositories.MessageRepositories;
-using RealEstate_Dapper_Api.Repositories.PopularLocationRepository;
-using RealEstate_Dapper_Api.Repositories.ProductImageRepositories;
-using RealEstate_Dapper_Api.Repositories.ProductRepository;
-using RealEstate_Dapper_Api.Repositories.PropertyAmenityRepositories;
-using RealEstate_Dapper_Api.Repositories.ServiceRepository;
-using RealEstate_Dapper_Api.Repositories.StatisticsRepositories;
-using RealEstate_Dapper_Api.Repositories.SubFeatureRepositories;
-using RealEstate_Dapper_Api.Repositories.TestimonialRepositories;
-using RealEstate_Dapper_Api.Repositories.ToDoListRepositories;
-using RealEstate_Dapper_Api.Repositories.WhoWeAreDetailRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options =>
@@ -27,26 +8,7 @@ builder.WebHost.ConfigureKestrel(options =>
 });
 
 // Add services to the container.
-builder.Services.AddTransient<Context>();
-builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-builder.Services.AddTransient<IProductRepository, ProductRepository>();
-builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
-builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
-builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
-builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
-builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
-builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddTransient<IStatisticsRepository, StatisticsRepository>();
-builder.Services.AddTransient<IContactRepository, ContactRepository>();
-builder.Services.AddTransient<IToDoListRepository, ToDoListRepository>();
-builder.Services.AddTransient<IStatisticRepository, StatisticRepository>();
-builder.Services.AddTransient<IChartRepository, ChartRepository>();
-builder.Services.AddTransient<IMessageRepository, MessageRepository>();
-builder.Services.AddTransient<IProductImageRepository, ProductImageRepository>();
-builder.Services.AddTransient<IAppUserRepository, AppUserRepository>();
-builder.Services.AddTransient<IPropertyAmenityRepository, PropertyAmenityRepository>();
-builder.Services.AddTransient<ILocationRepository, LocationRepository>();
-builder.Services.AddTransient<ISubFeatureRepository, SubFeatureRepository>();
+builder.Services.ContainerDependencies();
 
 builder.Services.AddCors(options =>
 {
